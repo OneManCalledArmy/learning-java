0d648d6e53716fb667de11fdf30a4c8289364a25
jacek-lapek <jacaplaca92@gmail.com>
2022-08-13 14:04:27 +0200

    vars

---
+++ intro/src/Main.java
@@ -0,0 +1,22 @@
+import javax.swing.plaf.synth.SynthPasswordFieldUI;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        System.out.println(" :) ");
+        System.out.println(" ehhe ");
+        
+        /* Variables:
+         * Declaraton + Assignment
+         * Initialization
+         */
+
+        int xint;
+        xint = 123;
+        long xlong = 100L;
+        float xfloat = 7f;
+
+        
+        }
+}
