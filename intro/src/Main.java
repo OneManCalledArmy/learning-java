1cea4cbe97eb92a018018cbccb3ca3fedcd7a42a
jacek-lapek <jacaplaca92@gmail.com>
2022-08-21 19:18:23 +0200

    xD

--- intro/src/Main.java
+++ intro/src/Main.java
@@ -1,12 +1,19 @@
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        String userInput = scanner.next();
-        System.out.println("User input:" + userInput);
+        // idx, expression, step
+        for (int i=10; i>0; i-=2) {
+            
+            if (i==10) {
+                System.out.println(i);
+            }
+            else {
+                System.out.println("Not 10 xD");
+            }
         
-        }
+        }        
+        
+    }
 }
