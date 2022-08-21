9167422dbe93a79463bfd809a54ba7f44171e762
jacek-lapek <jacaplaca92@gmail.com>
2022-08-21 19:22:31 +0200

    fixing

--- intro/src/Main.java
+++ intro/src/Main.java
@@ -3,16 +3,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // idx, expression, step
-        for (int i=10; i>0; i-=2) {
-            
-            if (i==10) {
-                System.out.println(i);
-            }
-            else {
-                System.out.println("Not 10 xD");
-            }
-        
         }        
         
     }
